b5dc3005408f8d90b8969f8c1c285f34f158a9f8
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-06 18:27:20 +0500

    Done!

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -5,6 +5,6 @@ public class FormulaCreator {
         int a, b;
         a= 1;
         b= 2;
-        System.out.println((9*Math.pow(a, 2)-5*b+2+a-7)*((a+b-4*a*b)/2));
+        System.out.println((9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2));
     }
 }
