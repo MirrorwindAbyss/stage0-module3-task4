86cd2e8a870b36f4f6939da568333eea4d980652
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-06 18:20:53 +0500

    Done!

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a, b, c, d;
+        a = 99;
+        b = 104;
+        c = 97;
+        d = 114;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
